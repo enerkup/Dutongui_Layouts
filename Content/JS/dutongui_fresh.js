38d29d92d0e2eadba0ab7e49cfe62e3714013c54
enerkup <distritomx@gmail.com>
2020-06-28 21:44:18 -0700

    Site ready to QA test

---
+++ Content/JS/dutongui_fresh.js
@@ -0,0 +1,8 @@
+
+$("#abre_menu").click(function(e){
+
+    $(".Introducing__Menu").toggleClass("show"); 
+    $("#abre_menu").toggleClass("fa-times"); 
+    $("body").toggleClass("no_scroll"); 
+
+}) 
